c8489ab95313802aee3a68b716e1afc4876dacbb
vigneshiva <53216559+vigneshiva@users.noreply.github.com>
2019-07-24 15:02:37 +0530

    Create alphabet.py

---
+++ alphabet.py
@@ -0,0 +1,5 @@
+ch = input(" ");
+if (ch =='a' or ch =='e' or ch =='i' or ch =='o' or ch =='u' or ch =='b' or ch =='c' or ch =='d' or ch =='f' or ch =='g' or ch =='h' or ch =='j' or ch =='k' or ch =='l' or ch =='m' or ch =='n' or ch=='p' or ch =='q' or ch =='r' or ch =='s' or ch =='t' or ch =='v' or ch =='w' or ch =='x' or ch=='y'or ch =='z') :
+  print("Alphabet");
+else :
+  print("No");
