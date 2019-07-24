102455a7c04e7b4e2c227ab3f932b305391cedff
vigneshiva <53216559+vigneshiva@users.noreply.github.com>
2019-07-24 12:45:02 +0530

    Update positiveornegative.py

--- positiveornegative.py
+++ positiveornegative.py
@@ -1,7 +1,4 @@
-a=int(input(""));
-if a>0:
-    print("Positive");
-elif a==0:
-    print("Zero");
-else:
-    print("Negative");
+a=int(input(" "));
+print("Positive") if a > 0
+else  print("Zero") if a == 0
+else print("Negative");
