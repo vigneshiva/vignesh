6203b07aa1259c7d0d18b10e1daf4d65d485f578
vigneshiva <53216559+vigneshiva@users.noreply.github.com>
2019-07-24 14:55:50 +0530

    Update vowelorconsonant.py

--- vowelorconsonant.py
+++ vowelorconsonant.py
@@ -1,5 +1,5 @@
 
-ch = char(input(" "));
+ch = input(" ");
 if (ch =='a' or ch =='e' or ch =='i' or ch =='o' or ch =='u'):
   print("Vowel");
 elif (ch =='b' or ch =='c' or ch =='d' or ch =='f' or ch =='g' or ch =='h' or ch =='j' or ch =='k' or ch =='l' or ch =='m' or ch =='n' or ch=='p' or ch =='q' or ch =='r' or ch =='s' or ch =='t' or ch =='v' or ch =='w' or ch =='x' or ch=='y'or ch =='z'):
