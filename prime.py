8935e7b53fca2d2e94252c2e201efb3a679c1919
vigneshiva <53216559+vigneshiva@users.noreply.github.com>
2019-07-31 15:06:50 +0530

    Update prime.py

--- prime.py
+++ prime.py
@@ -1,11 +1,11 @@
 a = int(input(" "));
-b = 0;
-if ( a > 0):
+if ( a > 1):
   for b in range(2,a):
     c = a % b;
     if ( c == 0):
-      print("yes");
-    else :
       print("no");
+      break
+  else:
+       print("yes");
 else:
   print("no");
