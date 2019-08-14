784275c9234c4241cd9954f0121da5e781b74b8a
vigneshiva <53216559+vigneshiva@users.noreply.github.com>
2019-08-14 15:06:20 +0530

    Create compare.py

---
+++ compare.py
@@ -0,0 +1,20 @@
+def check(a): 
+    i = 0 
+    j = i + 1
+    for i in a: 
+		for j in a:  
+         	if (a[i] = a[j]): 
+           			return False 
+    return True
+      
+      
+
+a = []
+for x in range(100000):
+    x = int(input())
+    a.append(x)     
+if(check(a): 
+    print(a[i])
+else: 
+    print"unique"
+  
